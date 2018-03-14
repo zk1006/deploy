26a451a223258ca04376eb79f55e76071403206a
zk <zk1006@live.cn>
2018-03-14 15:49:41 +0800

    测试

--- deploy.py
+++ deploy.py
@@ -1,11 +1,24 @@
-from flask import Flask
+from flask import Flask, render_template,request
+import shell
 
 app = Flask(__name__)
 
 
 @app.route('/')
 def hello_world():
-    return 'Hello World!'
+    return render_template('index.html')
+
+
+@app.route('/index')
+def index():
+    return render_template('index.html')
+
+
+@app.route('/command', methods=['GET', 'POST'])
+def command():
+    a=request.form["command"]
+    shell.shell(a)
+    return render_template('index.html')
 
 
 if __name__ == '__main__':
---
+++ shell.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+# encoding: utf-8
+
+# @Time         : 2018/3/14 下午3:04
+# @Author       : zk
+# @Mail         : zk1006@live.cn 
+# @File         : shell.py
+# @description  : shell命令执行控制器
+
+
+def shell(command: object) -> object:
+    return print(command)
